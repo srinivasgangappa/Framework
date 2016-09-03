c414521dda6cdaa8df6e5da11f30567f624b470b
srinivasgangappa <srinivasgangappa.87@gmail.com>
2016-09-03 18:09:07 +0530

    cxcv

---
+++ src/delta/main/CommonConstants.java
@@ -0,0 +1,12 @@
+package delta.main;
+
+public interface CommonConstants 
+{
+	public static final String configPptPath="./config/config.properties";
+	public static final  String scenarioPath="./scripts/scenarios.xlsx";
+	public static final  String reportFilePath="./Report/results.html";
+	public static final  String imageFolderPath="./ScreenShot";
+	public static final  String controllerPath="./scripts/scenarios.xlsx";
+	public static final  String suiteSheet="Condition";
+	
+}
